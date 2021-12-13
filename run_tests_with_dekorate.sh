5fd44b2215584b986ebabe493e17c21a6fb8141a
Jose <josecarvajalhilario@gmail.com>
2021-12-13 13:09:21 +0100

    Change startup conditions before running tests

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
+source scripts/waitFor.sh
+
 # deploy broker
 oc apply -f .openshiftio/amq.yaml
 oc new-app --template=amq-broker-72-basic -p APPLICATION_NAME=work-queue-broker -p IMAGE_STREAM_NAMESPACE=$(oc project -q) -p AMQ_NAME=work-queue-broker -p AMQ_PROTOCOL=amqp -p AMQ_USER=work-queue -p AMQ_PASSWORD=work-queue
+if [[ $(waitFor "work-queue-broker" "application") -eq 1 ]] ; then
+  echo "AMQ failed to deploy. Aborting"
+  exit 1
+fi
 
 # 1.- Deploy Frontend
 ./mvnw -s .github/mvn-settings.xml clean verify -pl frontend -Popenshift -Ddekorate.deploy=true
-
-# wait for the app to stand up
-sleep 30 # needed in order to bypass the 'Pending' state
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "spring-boot-messaging-work-queue-frontend" "app.kubernetes.io/name") -eq 1 ]] ; then
+  echo "Frontend failed to deploy. Aborting"
+  exit 1
+fi
 
 # 2.- Deploy Worker
 ./mvnw -s .github/mvn-settings.xml clean verify -pl worker -Popenshift -Ddekorate.deploy=true
-
-# wait for the app to stand up
-sleep 30 # needed in order to bypass the 'Pending' state
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "spring-boot-messaging-work-queue-worker" "app.kubernetes.io/name") -eq 1 ]] ; then
+  echo "Worker failed to deploy. Aborting"
+  exit 1
+fi
 
 # 3.- Run OpenShift Tests
 ./mvnw -s .github/mvn-settings.xml verify -pl tests -Popenshift-it
--- run_tests_with_s2i.sh
+++ run_tests_with_s2i.sh
@@ -2,21 +2,31 @@
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/messaging-work-queue-example}
 SOURCE_REPOSITORY_REF=${2:-sb-2.4.x}
 
+source scripts/waitFor.sh
+
 # deploy broker
 oc apply -f .openshiftio/amq.yaml
 oc new-app --template=amq-broker-72-basic -p APPLICATION_NAME=work-queue-broker -p IMAGE_STREAM_NAMESPACE=$(oc project -q) -p AMQ_NAME=work-queue-broker -p AMQ_PROTOCOL=amqp -p AMQ_USER=work-queue -p AMQ_PASSWORD=work-queue
+if [[ $(waitFor "work-queue-broker" "application") -eq 1 ]] ; then
+  echo "AMQ failed to deploy. Aborting"
+  exit 1
+fi
 
 # deploy frontend
 oc apply -f ./frontend/.openshiftio/application.yaml
 oc new-app --template=spring-boot-messaging-work-queue-frontend -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF -p SOURCE_REPOSITORY_DIR=frontend
+if [[ $(waitFor "spring-boot-messaging-work-queue-frontend" "app") -eq 1 ]] ; then
+  echo "Frontend failed to deploy. Aborting"
+  exit 1
+fi
 
 # deploy worker
 oc apply -f ./worker/.openshiftio/application.yaml
 oc new-app --template=spring-boot-messaging-work-queue-worker -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF -p SOURCE_REPOSITORY_DIR=worker
-
-sleep 30 # needed in order to bypass the 'Pending' state
-# wait for the app to stand up
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "spring-boot-messaging-work-queue-worker" "app") -eq 1 ]] ; then
+  echo "Worker failed to deploy. Aborting"
+  exit 1
+fi
 
 # launch the tests without deploying the application
 ./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it
