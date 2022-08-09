5bd1fdd4cd777b50a03bce41d1b6ed019a8ffd7a
Jose <josecarvajalhilario@gmail.com>
2022-08-09 12:09:03 +0200

    Bump Java to 11

--- run_tests_with_helm_in_ocp.sh
+++ run_tests_with_helm_in_ocp.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/messaging-work-queue-example}
 SOURCE_REPOSITORY_REF=${2:-sb-2.5.x}
+S2I_BUILDER_IMAGE_REPO=registry.access.redhat.com/ubi8/openjdk-11
+S2I_BUILDER_IMAGE_TAG=1.14
 
 source scripts/waitFor.sh
 
-helm install messaging ./helm --set frontend.route.expose=true --set frontend.s2i.source.repo=$SOURCE_REPOSITORY_URL --set frontend.s2i.source.ref=$SOURCE_REPOSITORY_REF --set worker.route.expose=true --set worker.s2i.source.repo=$SOURCE_REPOSITORY_URL --set worker.s2i.source.ref=$SOURCE_REPOSITORY_REF
+helm install messaging ./helm --set frontend.route.expose=true --set frontend.s2i.source.repo=$SOURCE_REPOSITORY_URL --set frontend.s2i.source.ref=$SOURCE_REPOSITORY_REF --set worker.route.expose=true --set worker.s2i.source.repo=$SOURCE_REPOSITORY_URL --set worker.s2i.source.ref=$SOURCE_REPOSITORY_REF --set frontend.s2i.builderImage.repo=$S2I_BUILDER_IMAGE_REPO --set frontend.s2i.builderImage.tag=$S2I_BUILDER_IMAGE_TAG --set worker.s2i.builderImage.repo=$S2I_BUILDER_IMAGE_REPO --set worker.s2i.builderImage.tag=$S2I_BUILDER_IMAGE_TAG
 if [[ $(waitFor "spring-boot-messaging-work-queue-worker" "app") -eq 1 ]] ; then
   echo "Worker failed to deploy. Aborting"
   exit 1
